b11bbcb8e5acf22f05266d79db6332039dd20c0a
k <jhkim@aimtog.co.kr>
2016-02-03 20:08:14 +0900

    delete rmi server start & client test

--- src/test/java/com/tistory/devilnangel/systeminfo/test/SystemInfoTest.java
+++ src/test/java/com/tistory/devilnangel/systeminfo/test/SystemInfoTest.java
@@ -1,14 +1,9 @@
 package com.tistory.devilnangel.systeminfo.test;
 
-import com.tistory.devilnangel.systeminfo.client.RmiSystemInfoClient;
 import com.tistory.devilnangel.systeminfo.common.Unit;
-import com.tistory.devilnangel.systeminfo.server.RmiSystemInfoServer;
 import com.tistory.devilnangel.systeminfo.system.SystemInfo;
 import org.hyperic.sigar.SigarException;
 import org.junit.Test;
-
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 
 /**
  * @author k, Created on 16. 1. 19.
@@ -35,16 +30,4 @@ public class SystemInfoTest {
         System.out.println(SystemInfo.isWindow());
     }
 
-    @Test
-    public void RMIServerInfoTest() throws RemoteException {
-        RmiSystemInfoServer s = new RmiSystemInfoServer();
-
-        s.main(null);
-    }
-
-    @Test
-    public void RMIClientTest() throws RemoteException, NotBoundException {
-        RmiSystemInfoClient sic = new RmiSystemInfoClient("localhost");
-        System.out.println(sic.getCpuInfo().getCpuBusy());
-    }
 }
