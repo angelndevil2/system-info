ba0e5f0a59e43913a6913dd5feb223d15e0c7862
k <jhkim@aimtog.co.kr>
2016-02-22 00:43:15 +0900

    remove lombok @Data

--- src/main/java/com/tistory/devilnangel/systeminfo/client/RmiSystemInfoClient.java
+++ src/main/java/com/tistory/devilnangel/systeminfo/client/RmiSystemInfoClient.java
@@ -2,7 +2,7 @@ package com.tistory.devilnangel.systeminfo.client;
 
 import com.tistory.devilnangel.systeminfo.common.IRmiCpuInfo;
 import com.tistory.devilnangel.systeminfo.common.IRmiUlimitInfo;
-import lombok.Data;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.rmi.NotBoundException;
@@ -11,11 +11,12 @@ import java.rmi.RemoteException;
 /**
  * @author k, Created on 16. 1. 30.
  */
-@Data
 @Slf4j
 public class RmiSystemInfoClient {
 
+    @Getter
     private final IRmiCpuInfo cpuInfo;
+    @Getter
     private final IRmiUlimitInfo ulimitInfo;
 
     public RmiSystemInfoClient(String host) throws RemoteException, NotBoundException {
